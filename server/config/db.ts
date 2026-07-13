1c43bd2ca4e9f57744cdf7a7890368383ca00883
unknown <ritika.chourasia0417@gmail.com>
2026-07-13 12:15:37 +0530

    backend server add

--- server/config/db.ts
+++ server/config/db.ts
@@ -2,12 +2,15 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || "");
-
-    console.log("✅ MongoDB Connected");
-    console.log(conn.connection.host);
-  } catch (error) {
-    console.error("❌ Database connection error:", error);
-    process.exit(1);
+    const mongoURI = process.env.MONGO_URI;
+    if (!mongoURI) {
+      throw new Error("MONGO_URI is not defined in the environment variables");
+    }
+    const conn = await mongoose.connect(mongoURI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`Host: ${conn.connection.host}`);
+  }catch (error) {
+    console.error(error);
+    process.exit(1); // Exit process with failure
   }
-};
+}
